5694f56904a252efde545a3e7e0491718f3c3cfb
brayanyt986 <88265256+brayanyt986@users.noreply.github.com>
2023-02-26 22:42:48 +0530

    Update 00_introduction.js

--- 00_introduction.js
+++ 00_introduction.js
@@ -1,1 +1,24 @@
-console.log("Hello")
+/*
+
+
+Introduction:
+
+When a user wants a computer to do any set of tasks , it needs to be fed with very precise instructions. Computer doesn’t understand normal languages like English , Hindi, Bengali etc. Therefore computer is a dumb machine, and needs to be fed with proper instructions. 
+Programming is an act of creating a program containing these instructions to tell the computer what to do.
+
+EcmaScript:
+
+This is a standard on which JavaScript is based. In the late 90s , when JavaScript was evolving as a language, people started to add their own features and documentation in the language to enhance it. Due to this, there might be chances that the language might show different behavior depending on the browser or the machine on which it runs. To solve this issue, EcmaScript was created which is a set of rules/standards that every JavaScript document needs to follow.
+
+Javascript and EcmaScript are used interchangably and JavaScript is very liberal in it’s execution, meaning it will try it’s best to throw minimum errors, and will throw an error only in extreme cases.
+
+Running JavaScript Code:
+
+There are three ways to run JavaScript code:
+
+i) In the browser, there is an inbuilt V8 engine that provides a runtime for JavaScript in the broswer. By clicking on inspect and going to console tab, it can be run on the broswer. Whenever we see a website whose data changes dynamically without reloading a webpage, we should know that it’s Javascript that is working in the background. 
+
+ii) We can use an external runtime like Nodejs to run Javascript outside the browser. Nodejs is a backend runtime environment which can be installed, and was created by placing the V8 engine inside a c++ program, so that it can be run independent of the browser. This was created by Ryan Dahl.
+
+iii) By using the script tag in HTML.
+*/
